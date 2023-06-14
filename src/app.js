e8ae45682bc393a19e286254189b46a26043ee92
Axel <amorales264@unab.edu.co>
2023-06-14 15:27:39 -0500

    IDK

--- src/app.js
+++ src/app.js
@@ -7,18 +7,6 @@ app.set("port", process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    next();
-});
 
 // Routes
 
