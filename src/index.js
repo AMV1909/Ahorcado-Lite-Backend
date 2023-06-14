01d51f933035913c7e99933fcaa49107a08eb124
Axel <amorales264@unab.edu.co>
2023-06-14 15:37:12 -0500

    Fix please

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ const io = new Server(server, { cors: { origin: "*", methods: "*" } });
 // Sockets
 gameSocket(io);
 
+// Middlewares
+server.prependListener("request", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+});
+
 // Start the server
 server.listen(app.get("port"), () => {
     console.log(`Server on port ${app.get("port")}`);
