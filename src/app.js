406a140f268143bcb8160fb0dfa2e72a2a96889c
Axel <amorales264@unab.edu.co>
2023-06-14 15:23:43 -0500

    Cors issue fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 
@@ -8,7 +7,18 @@ app.set("port", process.env.PORT || 3000);
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
 
 // Routes
 
